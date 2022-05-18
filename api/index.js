8b50844087f200349d8d6d56329915fb4deca754
Ryan <56096817+Youxiaaa@users.noreply.github.com>
2022-05-18 22:49:27 +0800

    update

--- api/index.js
+++ api/index.js
@@ -8,10 +8,12 @@ const service = axios.create({
 })
 
 service.interceptors.request.use((config) => {
+  console.log('送出前攔截測試')
   return config;
 }, (error) => Promise.reject(error))
 
 service.interceptors.response.use((response) => {
+  console.log('攔截測試')
   return response
 })
 
